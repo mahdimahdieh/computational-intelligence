fd21e27d3c58b5728e7bbf4abba92409f3f222c5
danial3000 <danstinhafman@gmail.com>
2025-05-30 20:23:40 +0430

    question 8 added
    question 8 added

--- Phase 3 Neural Network/q-part8/tex/Scripts/momentum_optimizer.py
+++
@@ -1,6 +0,0 @@
-def update_params(self, grads_w, grads_b):
-    for i in range(len(self.weights)):
-        self.vel_w[i] = self.momentum * self.vel_w[i] - self.lr * grads_w[i]
-        self.vel_b[i] = self.momentum * self.vel_b[i] - self.lr * grads_b[i]
-        self.weights[i] += self.vel_w[i]
-        self.biases[i] += self.vel_b[i]
