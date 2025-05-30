aff554bec5cbc2d593d0cc1a89e336f7af71c369
mahdimahdieh <mmahdieh90@gmail.com>
2025-05-30 12:08:32 +0330

    feat: implementing sigmoid activation function and binary cross entropy loss function

---
+++ Phase 3 Neural Network/task1-4/neuralnetwork.py
@@ -0,0 +1,13 @@
+import cupy as cp
+
+# Activation Functions
+def sigmoid(x):
+    return 1 / (1 + cp.exp(-x))
+
+# Loss Functions
+def binary_cross_entropy(y_true, y_pred, epsilon=1e-12):
+    # clip: ``maximum(minimum(a, a_max), a_min)`` to keep the prediction in range
+    y_pred = cp.clip(y_pred, epsilon, 1 - epsilon)
+    return -cp.mean(y_true * cp.log(y_pred) + (1 - y_true) * cp.log(1 - y_pred))
+
+
