8f96d26bfb1189e20eb8a612679d4da18b2b11b4
mahdimahdieh <mmahdieh90@gmail.com>
2025-05-30 12:04:14 +0330

    feat: cifar10 loader implemented

---
+++ Phase 3 Neural Network/task1-4/cifar10helper.py
@@ -0,0 +1,25 @@
+from torchvision import transforms, datasets
+import cupy as cp
+
+# Data Loading and Preprocessing
+def load_cifar10(binary=False, target_class=0):
+    transform = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+    ])
+
+    train_set = datasets.CIFAR10(root='./data', train=True, download=True, transform=transform)
+    test_set = datasets.CIFAR10(root='./data', train=False, download=True, transform=transform)
+
+    if binary:
+        # Binary classification (airplane vs rest)
+        train_set.targets = [0 if t == target_class else 1 for t in train_set.targets]
+        test_set.targets = [0 if t == target_class else 1 for t in test_set.targets]
+
+    # Convert to CuPy arrays
+    x_train = cp.array([x.numpy() for x, _ in train_set]).reshape(len(train_set), -1) # Flattening samples
+    y_train = cp.array(train_set.targets)
+    x_test = cp.array([x.numpy() for x, _ in test_set]).reshape(len(test_set), -1) # Flattening samples
+    y_test = cp.array(test_set.targets)
+
+    return x_train, y_train, x_test, y_test
